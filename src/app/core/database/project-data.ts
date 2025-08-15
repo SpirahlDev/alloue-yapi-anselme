6b9a232fa36b7545530fbdb7dabb7bb8d9f9be62
Anselme Alloue <anselmealloue@gmail.com>
2025-08-15 17:21:32 +0000

    Portfolio V1 done

--- src/app/core/database/project-data.ts
+++ src/app/core/database/project-data.ts
@@ -2,13 +2,13 @@ import { IProject, ProjectCategory } from "../interfaces/IProject";
 
 export const PROJECTS_DATA: IProject[] = [
   {
-    title: 'Plateforme de transfert sécurisé de fichiers interentreprises',
+    title: 'Plateforme sécurisée de transfert de fichiers interentreprises',
     placeholder: 'Transfert de fichiers sensibles avec chiffrement PGP et envoi SFTP',
     description:
       `Cette application web sécurisée permet à des entreprises d’échanger des fichiers sensibles avec chiffrement PGP de bout en bout et transfert automatisé vers un serveur SFTP. 
        Chaque utilisateur possède sa clé PGP unique, gérée directement dans l’interface Angular, et le chiffrement est réalisé côté client via OpenPGP.js. 
        Le backend, en Node.js avec Express, gère l’authentification, l’administration des clés et les transferts SFTP, tout en respectant le RGPD. 
-       J’ai travaillé sur la conception de l’interface et la mise en place des mécanismes de sécurité et de traçabilité.`,
+      `,
     category: ProjectCategory.WEB_APPLICATION,
     start_date: new Date('2025-04-01'),
     end_date: new Date('2025-06-31'),
@@ -34,29 +34,26 @@ export const PROJECTS_DATA: IProject[] = [
       '/assets/projects/digidoc/screen-2.png',
       '/assets/projects/digidoc/screen-1.png'
     ],
-    tags: ['Angular', 'Quarkus', 'Digital Signature', 'Security', 'Real-time', 'Cryptoneo', 'SSE'],
+    tags: ['Angular', 'Quarkus', 'Real-time', 'Cryptographie', 'SSE'],
   },
   {
     title: 'REX Hotel - Gestion de réservation',
     placeholder: 'Modélisation et conception d’un système de gestion hôtelière haut de gamme',
-    description:
-      `Développé pour le REX HOTEL, établissement 4 étoiles à Yamoussoukro, ce système permet la réservation en ligne avec calendrier de disponibilités, tarification dynamique et paiement sécurisé. 
-       Côté personnel, un tableau de bord centralise les opérations : check-in/check-out, facturation automatique, gestion des services (spa, restauration, parking). 
-       Conçu avec Laravel et MySQL, il optimise la gestion et l’expérience client dans un contexte haut de gamme.`,
+    description: 
+  `Développé pour le REX HOTEL, établissement 4 étoiles à Yamoussoukro, ce système permet la réservation en ligne avec calendrier de disponibilités, tarification dynamique et paiement sécurisé. 
+   Pour le personnel, un tableau de bord centralise les opérations : check-in/check-out, facturation et gestion des services proposés par l’hôtel (bar, restauration) directement depuis le site web.`,
     category: ProjectCategory.WEB_APPLICATION,
     start_date: new Date('2025-05-01'),
     end_date: new Date('2025-06-28'),
     link: '',
     screenshots: ['/assets/projects/rexhotel/screen-1.png'],
-    tags: ['Laravel', 'REST API', 'MySQL','Booking System', 'Hotel Management', 'Payment Integration'],
+    tags: ['Laravel', 'REST API', 'MySQL','Angular'],
   },
   {
     title: 'AbidjanActu',
     placeholder: 'Site d’actualités locales et de diffusion d’informations',
-    description:
-      `AbidjanActu est un site d’actualités développé pour la diffusion d’informations en Côte d’Ivoire. 
-       Construit avec Laravel et Tailwind CSS, il intègre un CMS complet pour la gestion des articles, catégories et commentaires, avec modération et optimisation SEO. 
-       Un espace administrateur permet également de gérer les publicités et d’analyser les performances grâce à un tableau de bord analytique.`,
+    description: 
+    `AbidjanActu est un site d’actualités conçu pour diffuser efficacement l’information en Côte d’Ivoire. Développé avec Laravel et Tailwind CSS, il intègre un CMS complet pour la gestion des articles, des catégories et des commentaires, avec modération et optimisation SEO. Un espace administrateur permet également de gérer les publicités et d’analyser les performances via un tableau de bord analytique, offrant ainsi une solution robuste et professionnelle pour la publication et le suivi des contenus.`,
     category: ProjectCategory.WEB_APPLICATION,
     start_date: new Date('2025-04-01'),
     end_date: new Date('2025-06-30'),
@@ -69,25 +66,23 @@ export const PROJECTS_DATA: IProject[] = [
   },
   {
     title: 'Solution web de gestion de contrats signés électroniquement',
-    placeholder: 'Consultation et gestion en ligne des contrats d’abonnés',
+    placeholder: 'Consultation et gestion en ligne de contrats',
     description:
-      `Conçue pour Cryptoneo, cette solution permet aux abonnés d’un opérateur télécom d’accéder à leurs contrats signés électroniquement. 
-       L’interface, développée en Angular, offre une navigation simple et sécurisée, tandis que le backend Quarkus gère l’authentification, la récupération et la validation des documents. 
-       Ce projet a permis d’améliorer la transparence et l’accessibilité des informations contractuelles pour des milliers d’abonnés.`,
+    `Conçue pour Cryptoneo, cette solution permet aux utilisateurs des plateformes de signature de l'entreprise (B2B & B2C) d’accéder facilement à leurs contrats signés électroniquement. 
+    L’interface, développée en Angular, offre une navigation fluide et sécurisée, tandis que le backend Quarkus assure l’authentification, la récupération et la validation des documents. 
+    Ce projet a renforcé la transparence et l’accessibilité des informations contractuelles pour des milliers d’abonnés.`,
     category: ProjectCategory.WEB_APPLICATION,
     start_date: new Date('2025-05-01'), // À ajuster selon ton CV
     end_date: new Date('2025-07-01'),
     link: '',
     screenshots: ['/assets/projects/suivi-contrat/screen-1.png'],
-    tags: ['Angular', 'Quarkus', 'Horodatage', 'Security'],
+    tags: ['Angular', 'Quarkus', 'Horodatage'],
   },
   {
     title: 'Lifemag',
     placeholder: 'Plateforme de distribution de magazines et d’actualités',
     description: 
-      `Développée pour le groupe Meduim X - Voodoo Group, Lifemag est une plateforme web permettant aux éditeurs de publier des magazines et aux lecteurs de les consulter en ligne. 
-       L’outil intègre un système de gestion des rôles, la sécurisation des PDF et images de couverture, ainsi qu’un back-office complet pour la gestion des publications. 
-       Réalisé avec Laravel et Angular, ce projet a renforcé mon expertise en gestion de contenus numériques et en sécurité de documents.`,
+      `Développée pour Meduim X, filiale de Voodoo Group, Lifemag est une plateforme web qui permet aux éditeurs de publier leurs magazines et aux lecteurs de les consulter en ligne. La solution intègre un système complet de gestion des rôles, la sécurisation des PDF et des images de couverture, ainsi qu’un back-office puissant pour gérer l’ensemble des publications. Conçue avec Laravel et Angular, cette réalisation a renforcé mon expertise en gestion de contenus numériques et en sécurisation de documents tout en consolidant mes compétences fullstack.`,
     category: ProjectCategory.WEB_APPLICATION,
     start_date: new Date('2024-12-01'),
     end_date: new Date('2025-02-01'),
@@ -96,7 +91,7 @@ export const PROJECTS_DATA: IProject[] = [
       '/assets/projects/lifemag/lifemag-screen-1.png',
       '/assets/projects/lifemag/lifemag-screen-2.png',
     ],
-    tags: ['Laravel', 'Angular', 'MySQL', 'CMS', 'PDF Management'],
+    tags: ['Laravel', 'Angular', 'MySQL', 'CMS', 'PDF Management','REST API'],
   },
   {
     title: 'MonBarra',
@@ -106,7 +101,7 @@ export const PROJECTS_DATA: IProject[] = [
        L’application permet aux chercheurs d’emploi de créer des profils détaillés et aux employeurs de publier leurs offres. 
        Un moteur de recherche intelligent filtre les résultats selon les compétences, l’expérience et la localisation. 
        Les utilisateurs bénéficient d’un système de messagerie intégré et les recruteurs disposent de tableaux de bord analytics. 
-       Développée avec Angular pour le frontend et Laravel/MySQL côté serveur, la solution offre une expérience fluide et performante.`,
+       `,
     category: ProjectCategory.WEB_APPLICATION,
     start_date: new Date('2024-09-01'),
     end_date: new Date('2024-12-01'),
@@ -122,48 +117,40 @@ export const PROJECTS_DATA: IProject[] = [
     title: 'Lalexandra',
     placeholder: 'Boutique en ligne de cosmétiques avec gestion complète des ventes et produits',
     description: 
-      `Dans le cadre d’un projet d’étude, j’ai conçu et développé Lalexandra, une boutique e-commerce spécialisée dans la vente de cosmétiques. 
-       Les clientes peuvent parcourir le catalogue, créer un compte, remplir leur panier et régler leurs achats via CinetPay. 
+      `Lalexandra s'inscrivait dans un projet d'étude, où j'ai conçu et développé une boutique e-commerce spécialisée dans la vente de cosmétiques. 
+       Les clientes peuvent parcourir le catalogue, créer un compte, remplir leur panier et régler leurs achats. 
        Un back-office complet permet la gestion des produits, des stocks et des commandes. 
-       Réalisé avec Java Servlet, JSP et MySQL, ce projet m’a permis de travailler sur l’intégration d’un paiement en ligne et la structuration d’un site marchand.`,
+       Réalisé avec Java Servlet, JSP et MySQL, ce projet m’a permis de maîtriser les fondamentaux de Java Enterprise Edition et de démontrer ma capacité à concevoir des applications robustes, évolutives et professionnelles.`,
     category: ProjectCategory.ECOMMERCE,
     start_date: new Date('2024-02-01'),
     end_date: new Date('2024-06-30'),
-    link: 'https://github.com/anselme-alloue/lalexandra',
     screenshots: [
       '/assets/projects/lalexandra/screen-1.png',
       '/assets/projects/lalexandra/screen-2.png',
     ],
-    tags: ['Java Servlet', 'Tomcat', 'JSP', 'MySQL', 'E-commerce', 'JWT'],
+    tags: ['Java Servlet', 'Tomcat', 'JSP', 'MySQL'],
   },
 
   {
     title: 'KelenFila - Plateforme de vente aux enchères',
     placeholder: 'Système complet d’enchères en ligne',
     description:
-      `KelenFila est une plateforme d’enchères en ligne permettant aux utilisateurs de mettre en vente ou d’acheter des articles via un système de mises en temps réel. 
-       Développée avec Angular et Laravel, elle intègre la gestion des utilisateurs, la mise en ligne d’articles avec photos multiples, 
-       le suivi en direct des enchères via WebSockets, et un système de paiement intégré. 
-       Ce projet m’a permis de combiner développement frontend, backend et gestion en temps réel pour offrir une expérience interactive fluide.`,
+      `KelenFila est une plateforme d’enchères en ligne qui permet aux utilisateurs de vendre ou d’acheter des articles via un système de mises en temps réel. La solution intègre la gestion complète des utilisateurs, la publication d’articles avec photos multiples, le suivi et l’enchérissement en direct grâce aux WebSockets, ainsi qu’un système de paiement intégré. Ce projet m’a permis de combiner avec succès développement frontend et backend tout en maîtrisant la gestion en temps réel, offrant ainsi une expérience utilisateur interactive et fluide.`,
     category: ProjectCategory.ECOMMERCE,
-    start_date: new Date('2023-03-01'),
+    start_date: new Date('2023-02-01'),
     end_date: new Date('2023-05-31'),
     link: '',
     screenshots: [
       '/assets/projects/kelen-fila/screen-1.png',
       '/assets/projects/kelen-fila/screen-2.png'
     ],
-    tags: [ 'PHP', 'WebSockets', 'Javascript','Real-time', 'Payment System', 'Auction', 'E-commerce', 'JavaScript'],
+    tags: [ 'PHP', 'WebSockets', 'Javascript','Real-time', 'Payment System'],
   },
   {
     title: 'Plateforme de prestation de service de livraison',
     placeholder: 'Plateforme web pour recevoir et gérer les demandes de livraison',
     description:
-      `Projet client que j’ai conçu et développé de bout en bout. 
-       Cette plateforme connecte des particuliers ou commerçants souhaitant envoyer des colis avec des livreurs disponibles. 
-       Côté client, l’interface permet de créer une demande, calculer le coût, suivre la livraison en temps réel et effectuer le paiement. 
-       Côté livreur, une application mobile web gère les courses, propose un suivi GPS et conserve l’historique des livraisons. 
-       Développée avec Angular et Laravel, cette solution inclut des notifications push et un système de gestion automatisée des courses.`,
+      `Projet client que j’ai conçu et développé de bout en bout. Cette plateforme permet aux utilisateurs de soumettre des demandes de livraison à l’entreprise, qui se charge ensuite de collecter et de distribuer leurs colis de manière sécurisée et efficace.`,
     category: ProjectCategory.WEB_APPLICATION,
     start_date: new Date('2023-05-01'),
     end_date: new Date('2023-07-01'),
@@ -180,7 +167,7 @@ export const PROJECTS_DATA: IProject[] = [
       Bien qu’encore en cours de développement, ce projet bénéficie d’une architecture soignée et d’un design pensé pour durer, 
       avec pour objectif de proposer une expérience à forte valeur ajoutée dès son lancement.`,
     category: ProjectCategory.WEB_APPLICATION,
-    start_date: new Date('2025-01-01'), // à ajuster selon le vrai début
+    start_date: new Date('2025-04-01'), // à ajuster selon le vrai début
     link: '',
     screenshots: [
       '/assets/projects/1erscore/1erscore.png',
@@ -209,7 +196,7 @@ export const PROJECTS_DATA: IProject[] = [
   },
   {
     title: 'mesTchams',
-    placeholder: 'Application sociale et de rencontre nouvelle génération',
+    placeholder: 'Application sociale et de rencontre',
    description:
     `Ce projet est un prototype d'application sociale et de rencontre innovante, 
     offrant un système d’interaction progressif avec profils détaillés, 
@@ -217,7 +204,6 @@ export const PROJECTS_DATA: IProject[] = [
     Elle se distingue par des effets personnalisables sur les messages, 
     une recherche avancée et un fil d’actualité ciblé, 
     favorisant des échanges authentiques et engageants.`,
-
     category: ProjectCategory.MOBILE_APP,
     start_date: new Date('2024-07-01'), // à ajuster selon le vrai début
     end_date: new Date('2025-08-01'), // encore en cours
@@ -235,8 +221,8 @@ export const PROJECTS_DATA: IProject[] = [
     description:
       'Application mobile destinée aux livreurs souhaitant utiliser nos services pour exercer leur activité. Elle offre un parcours d’inscription simplifié, la gestion de profil, le suivi en temps réel des livraisons via carte interactive, et la consultation de l’historique des courses et paiements. J’ai principalement assuré la conception complète de l’expérience utilisateur et des interfaces.',
     category: ProjectCategory.MOBILE_APP,
-    start_date: new Date('2023-02-01'), // à ajuster si nécessaire
-    end_date: new Date('2023-03-15'),   // à ajuster si nécessaire
+    start_date: new Date('2024-08-01'), // à ajuster si nécessaire
+    end_date: new Date('2024-09-15'),   // à ajuster si nécessaire
     link: '',
     screenshots: [
       '/assets/projects/zoCash/screen-1.png',
